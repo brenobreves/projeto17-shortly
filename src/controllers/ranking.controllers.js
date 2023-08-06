56789001c2528450f94ffb26a0c19cb4a9d43f76
breno.breves <brenocsb@dcc.ufrj.br>
2023-08-06 19:57:51 -0300

    GET Ranking implementado

--- src/controllers/ranking.controllers.js
+++ src/controllers/ranking.controllers.js
@@ -1,5 +1,15 @@
 import {db} from "../database/database.connection.js"
 
 export async function getRanking(req,res){
-    res.send("getRanking")
+    try {
+        const ranking = await db.query(`
+        SELECT owner AS id, u.name, COUNT(*) as "linksCount", SUM("visitCount") as "visitCount"  
+        FROM urls LEFT JOIN users u ON owner=u.id 
+        GROUP BY owner, u.name 
+        ORDER BY "visitCount" DESC 
+        LIMIT 10;`)
+        res.send(ranking.rows)
+    } catch (err) {
+        res.status(500).send(err.message)         
+    }
 }
